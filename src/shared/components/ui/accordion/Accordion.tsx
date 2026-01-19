0f44bc7c3ebd14c54cb2875586a86c45fbc7b89a
Wi hyun Jeong <wihyunjeong@Wiui-noteubug.local>
2026-01-19 17:35:12 +0900

    accordion 컴포넌트 작업 시작중

--- src/shared/components/ui/accordion/Accordion.tsx
+++ src/shared/components/ui/accordion/Accordion.tsx
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
-import * as React from "react";
-import * as AccordionPrimitive from "@radix-ui/react-accordion";
+import * as React from 'react';
+import * as AccordionPrimitive from '@radix-ui/react-accordion';
 
 export default function Accordion({
   ...props
 }: React.ComponentProps<typeof AccordionPrimitive.Root>) {
   return <AccordionPrimitive.Root data-slot="accordion"
-{...props} />;
+	{...props} />;
 }
